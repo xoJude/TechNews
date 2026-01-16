c46d29e611e2be7e836604ddedc7512ecad122b9
Jude Gigon <jude.gigon@icloud.com>
2026-01-16 18:09:06 +0100

    fix useNavigate import

--- front-end/src/pages/Contact.js
+++ front-end/src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react';
+import { useNavigate } from "react-router-dom";
+
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import "../styles/style-connexion.css";
